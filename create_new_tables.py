5daeb2ef821b5f2efd43d92af5f36420607f51ac
Vadim-Valiullin <113513063+Vadim-Valiullin@users.noreply.github.com>
2022-10-24 22:21:50 +0500

    Update create_new_tables.py
    Нормализовал таблицу в 2 сущности: shelter, cats_table.

--- create_new_tables.py
+++ create_new_tables.py
@@ -8,7 +8,7 @@ def connection():
                                       password="123456",
                                       host="127.0.0.1",
                                       port="5432",
-                                      database="cat_shelter")
+                                      database="cat_shelter_1")
         connection.autocommit = True
         print('Connection Done')
         return connection
@@ -20,13 +20,6 @@ def connection():
 def create_new_tables(cursor):
     try:
         cursor.execute("""
-            SELECT DISTINCT animal_type
-            INTO animal_type_table
-            FROM cat_shelter;
-            
-            ALTER TABLE animal_type_table
-            ADD COLUMN id_animal_type SERIAL PRIMARY KEY;
-            
             SELECT DISTINCT breed
             INTO breed_table
             FROM cat_shelter;
@@ -34,7 +27,7 @@ def create_new_tables(cursor):
             ALTER TABLE breed_table
             ADD COLUMN id_breed SERIAL PRIMARY KEY;
             
-            SELECT DISTINCT color1, color2
+            SELECT DISTINCT color
             INTO color_table
             FROM cat_shelter;
             
@@ -55,50 +48,53 @@ def create_new_tables(cursor):
             ALTER TABLE outcome_type_table
             ADD COLUMN id_outcome_type SERIAL PRIMARY KEY;
             
-            SELECT age_upon_outcome, animal_id, animal_type_table.id_animal_type, name, breed_table.id_breed, color_table.id_color, date_of_birth, outcome_subtype_table.id_outcome_subtype, outcome_type_table.id_outcome_type
-            INTO new_shelter
+            SELECT DISTINCT animal_id, animal_type, name, breed_table.id_breed, color_table.id_color, date_of_birth
+            INTO cats_table
             FROM cat_shelter
-            JOIN animal_type_table ON animal_type_table.animal_type = cat_shelter.animal_type
             JOIN breed_table ON breed_table.breed = cat_shelter.breed
             JOIN color_table ON color_table.color = cat_shelter.color
+            GROUP BY animal_id, animal_type, name, breed_table.id_breed, color_table.id_color, date_of_birth;
+            
+            SELECT age_upon_outcome, cats_table.animal_id, outcome_subtype_table.id_outcome_subtype, 
+            outcome_type_table.id_outcome_type, outcome_month, outcome_year
+            INTO shelter
+            FROM cat_shelter
+            JOIN cats_table ON cats_table.animal_id = cat_shelter.animal_id
             JOIN outcome_subtype_table ON outcome_subtype_table.outcome_subtype = cat_shelter.outcome_subtype
             JOIN outcome_type_table ON outcome_type_table.outcome_type = cat_shelter.outcome_type
-            GROUP BY age_upon_outcome, animal_id, animal_type_table.id_animal_type, name, breed_table.id_breed, color_table.id_color, date_of_birth, outcome_subtype_table.id_outcome_subtype, outcome_type_table.id_outcome_type;
+            GROUP BY age_upon_outcome, cats_table.animal_id, outcome_subtype_table.id_outcome_subtype, outcome_type_table.id_outcome_type, outcome_month, outcome_year;
             
-            ALTER TABLE new_shelter
+            ALTER TABLE shelter
             ADD COLUMN id_cat_shelter SERIAL PRIMARY KEY;
             
-            ALTER TABLE new_shelter
-            ADD CONSTRAINT fk_cat_type FOREIGN KEY (id_animal_type) REFERENCES animal_type_table (id_animal_type);
-            
-            ALTER TABLE new_shelter
+            ALTER TABLE cats_table
             ADD CONSTRAINT fk_cat_breed FOREIGN KEY (id_breed) REFERENCES breed_table (id_breed);
             
-            ALTER TABLE new_shelter
+            ALTER TABLE cats_table
             ADD CONSTRAINT fk_cat_color FOREIGN KEY (id_color) REFERENCES color_table (id_color);
             
-            ALTER TABLE new_shelter
+            ALTER TABLE shelter
             ADD CONSTRAINT fk_cat_subtype FOREIGN KEY (id_outcome_subtype) REFERENCES outcome_subtype_table (id_outcome_subtype);
            
-            ALTER TABLE new_shelter
+            ALTER TABLE shelter
             ADD CONSTRAINT fk_cat_outcome_type FOREIGN KEY (id_outcome_type) REFERENCES outcome_type_table (
             id_outcome_type);
             
-            CREATE USER test_1;
+            CREATE USER Bonnie;
 
-            GRANT SELECT ON cat_shelter TO test_1;
+            GRANT SELECT ON shelter TO Bonnie;
             
-            SELECT * FROM information_schema.table_privileges WHERE grantee='test_1';
+            SELECT * FROM information_schema.table_privileges WHERE grantee='Bonnie';
             
-            ALTER USER test_1 WITH PASSWORD '1234';
+            ALTER USER Bonnie WITH PASSWORD '1234';
             
-            CREATE USER Robin;
+            CREATE USER Clide;
 
-            GRANT SELECT, INSERT, UPDATE ON cat_shelter TO Robin;
+            GRANT SELECT, INSERT, UPDATE ON shelter TO Clide;
             
-            SELECT * FROM information_schema.table_privileges WHERE grantee='Robin';
+            SELECT * FROM information_schema.table_privileges WHERE grantee='Clide';
             
-            ALTER USER Robin WITH PASSWORD '1234'
+            ALTER USER Clide WITH PASSWORD '1234'
         """)
         print('Таблица создана')
     except (Exception, Error) as error:
